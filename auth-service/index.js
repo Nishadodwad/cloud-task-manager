2722bc954b2517f3c2a7f04cf3c3a93e87eca3f1
Nisha V D <nishadodawad5@gmail.com>
2026-01-12 20:13:36 +0530

    Fix CORS for Render frontend

--- auth-service/index.js
+++ auth-service/index.js
@@ -5,7 +5,12 @@ require("dotenv").config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"]
+}));
+
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("Auth DB connected"))
